cc3ab1fbcfeb2b558b3510f1ad6f7d1d51f65052
= <pakoo_15@hotmail.com>
2022-05-08 19:05:52 -0500

    Configure material ui for views

--- world/src/index.tsx
+++ world/src/index.tsx
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { App } from './App';
 import './index.css';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './theme/theme';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -11,7 +13,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+    <ThemeProvider theme={theme}>
       <App />
+    </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
